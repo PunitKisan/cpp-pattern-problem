8bf52c741ab2c10b9ae963f86aabe62d7827129d
PuniKisan <punitcode18@gmail.com>
2022-07-28 23:18:16 +0530

    Added solution for pattern 1

---
+++ pattern1.cpp
@@ -0,0 +1,22 @@
+/*
+1. Print the pattern
+    *****
+    *****
+    *****
+    *****
+ */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 5; j++)
+        {
+            cout << '*';
+        }
+        cout << '\n';
+    }
+    return 0;
+}
