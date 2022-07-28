3f2adea628af04d1eaa560a34fd1ca69d6c3f597
PuniKisan <punitcode18@gmail.com>
2022-07-29 00:47:37 +0530

    Added solution for pattern 2

---
+++ pattern2.cpp
@@ -0,0 +1,33 @@
+/*
+2. Print the pattern
+    *****
+    *   *
+    *   *
+    *   *
+    *****
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 5; j++)
+        {
+            if (i == 1 || i == 5|| j == 1 || j == 5)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+
+        cout << '\n';
+    }
+
+    return 0;
+}
