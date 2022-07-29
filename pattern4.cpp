e3ed42afaf249c5b019bf9e61d6ee7590ba9fefb
PuniKisan <punitcode18@gmail.com>
2022-07-30 00:29:46 +0530

    Added solution for pattern 4

--- pattern4.cpp
+++ pattern4.cpp
@@ -1,23 +1,21 @@
 /*
-3. Print the pattern
-    *
-    **
-    ***
+4. Print the pattern
     ****
-    *****
+    ***
+    **
+    *
 */
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    for (int i = 1; i <= 5; i++)
+    for (int i = 1; i <= 4; i++)
     {
-        for (int j = 1; j <= i; j++)
+        for (int j = 4; j >= i; j--)
         {
             cout << '*';
         }
         cout << '\n';
     }
-    return 0;
 }
